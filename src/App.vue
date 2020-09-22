1f2fb37383b4d0ee8484cdf0cd80313b59a8cc1e
AkatorMr <marck_sparkle@hotmail.com>
2020-09-22 13:45:40 -0300

    Inicio

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <fecha />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Fecha from "./components/Fecha"
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Fecha
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Fecha.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="">
+    <input @keypress="key_press" @keydown="key_down" v-model="fecha">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Fecha',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      re:null,
+      fecha:""
+    }
+  },
+  methods: {
+    key_press:function(e){
+      //console.log(e);
+      if (e.stopPropagation && !this.re.test(e.key)) {
+        e.stopPropagation();
+        e.preventDefault();
+
+      }else{
+        console.log(this.fecha.length);
+        console.log(this.fecha);
+        if(this.fecha.length==0){
+          // no tiene nada
+          if(e.key=="/"){
+            e.stopPropagation();
+            e.preventDefault();
+          }
+        }else{
+          if(this.fecha.length==2){
+            if(e.key!="/"){
+              if(this.fecha.charAt(this.fecha.length-1)!="/")
+              this.fecha+="/";
+            }
+          }
+        //es un digito o la barra inversa
+        let sp = this.fecha.split("/");
+        if(sp.length==0){
+          //no tiene /
+
+        }else {
+          //Tiene al menos una
+        }
+        }
+
+      }
+     
+    },
+    key_down:function(e){
+      //console.log(this.re.test(e.key));
+      //return true;
+    }
+  },
+  mounted(){
+    console.log("Mounted");
+    this.re = new RegExp("[0-9/]");
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
