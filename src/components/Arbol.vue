c013a561f7ec51a016a68c7e702e8454f0557d8e
marcos.pereson <marck_sparkle@hotmail.com>
2020-11-09 22:16:50 -0300

    anda2

--- src/components/Arbol.vue
+++ src/components/Arbol.vue
@@ -23,7 +23,7 @@ export default {
       re: null,
       fecha: "",
       error: false,
-      mostrar: false
+      mostrar: true
     };
   },
   methods: {
@@ -42,7 +42,10 @@ export default {
 <style scoped>
 .arbol {
   /*margin-left: 30px;*/
-  margin-top: 8px;
+  padding-top: 8px;
+  border-left-color: black;
+  border-left-style: dashed;
+  border-left-width: 1px;
 }
 .desplegable {
   padding-left: 30px;
@@ -53,14 +56,28 @@ export default {
   color: #2c3e50;
   width: 0;
   height: 7px;
-  border-left-color: black;
+  /* border-left-color: black;
   border-left-style: dashed;
-  border-left-width: 1px;
+  border-left-width: 1px; */
   border-bottom: 1px dashed black;
   content: "";
   margin-bottom: 0;
   margin-left: 0;
   float: left;
   padding-right: 11px;
+}
+.ele2 {
+  float: left;
+}
+.ele2::before {
+  content: "";
+  width: 0px;
+  height: 0px;
+
+  border-right: 50px solid transparent;
+  border-top: 50px solid transparent;
+  border-left: 1px dashed darkblue;
+  border-bottom: 1px dashed darkblue;
+  float: left;
 }
 </style>
